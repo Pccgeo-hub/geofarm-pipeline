2ef831a26f19253ced9151f2d2e9d555dca904e4
Pccgeo-hub <pchukwu757@gmail.com>
2025-10-21 21:04:27 +0200

    Stop tracking local env; add .gitignore

--- mamba/Library/include/test_data.hpp
+++
@@ -1,1 +0,0 @@
-#define TEST_DATA_DIRECTORY "D:/bld/nlohmann_json_1723651555814/work/json_test_data"
--- mamba/condabin/activate.bat
+++
@@ -1,5 +0,0 @@
-@REM Copyright (C) 2021 QuantStack
-@REM SPDX-License-Identifier: BSD-3-Clause
-
-@CALL "%~dp0..\condabin\mamba_hook.bat"
-mamba activate %*
